96ad0cf4f8cdbbb5c85a710feff3fe4b878fb952
HAUNC_PC\ADMIN <nguyenconghau188@gmail.com>
2021-07-05 06:08:14 +0700

    [food_order] add react context + reducer

--- food_order/src/App.js
+++ food_order/src/App.js
@@ -1,8 +1,9 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShow, setCartIsShow] = useState(false);
@@ -16,13 +17,13 @@ function App() {
   };
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShow && <Cart onHideCart={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
--- food_order/src/components/Cart/CartButton.js
+++ food_order/src/components/Cart/CartButton.js
@@ -1,16 +1,23 @@
-import { Fragment } from "react";
+import { useContext } from "react";
 
-import classes from "./CartButton.module.css";
 import CartIcon from "./CartIcon";
+import CartContext from "../../store/cart-context";
+import classes from "./CartButton.module.css";
 
 const CartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalItemsInCart = cartCtx.items.reduce((total, item) => {
+    return total + item.totalAmount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{totalItemsInCart}</span>
     </button>
   );
 };
